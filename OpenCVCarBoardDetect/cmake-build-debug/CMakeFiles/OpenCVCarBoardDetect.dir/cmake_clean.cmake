4b1f013094e9101d55d731612cf7313c8f82cf48
lixiaoxu <lixiaoxu@askingdata.com>
2019-10-15 11:29:30 +0800

    add car-board detect

---
+++ OpenCVCarBoardDetect/cmake-build-debug/CMakeFiles/OpenCVCarBoardDetect.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenCVCarBoardDetect.dir/main.cpp.o"
+  "OpenCVCarBoardDetect.pdb"
+  "OpenCVCarBoardDetect"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenCVCarBoardDetect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
